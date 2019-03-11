e5e4ae1c336ba21ff8cc5a9acbd2efbe5c6756c1
Aj Shaw <ajshawit@gmail.com>
2019-03-11 22:30:00 +1100

    Planning for features

--- script.php
+++ script.php
@@ -5,7 +5,7 @@ $shortOptions = "u:p:h:";
 $longOptions = array("file:", "help::", "dry_run::", "create_table::");
 
 if (array_key_exists("help", getopt($shortOptions, $longOptions))) {
-	print "--file [csv file name] - Name of the CSV to be parsed \n";
+	print "--file [csv file name] - Name of the CSV to be parsed (must be in same directory as script)\n";
 	print "--create_table - Build MySQL \n";
 	print "--dry_run - run all queries entered inputed but make no alterations to the database \n";
 	print "-u - MySQL username \n";
@@ -13,8 +13,14 @@ if (array_key_exists("help", getopt($shortOptions, $longOptions))) {
 	print "-h - MySQL host \n";
 	print "--help - Show this message\n";
 }
+elseif (array_key_exists("dry_run", getopt($shortOptions, $longOptions))) {
+	//perform function without modifying database
+}
+elseif (array_key_exists("create_table", getopt($shortOptions, $longOptions))) {
+	//create table
+}
 else {
-	var_dump($options);
+	//run function that modifies database
 }
 
 ?>
