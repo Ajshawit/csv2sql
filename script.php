9025c9dc711ab9dfa9f6c7ba224a187a1314ae4e
Aj Shaw <ajshawit@gmail.com>
2019-03-11 22:43:38 +1100

    Update script.php
    Reducing duplications and adding functions to connect and disconnect from database

--- script.php
+++ script.php
@@ -4,7 +4,9 @@ $shortOptions = "u:p:h:";
 
 $longOptions = array("file:", "help::", "dry_run::", "create_table::");
 
-if (array_key_exists("help", getopt($shortOptions, $longOptions))) {
+$options = getopt($shortOptions, $longOptions);
+
+if (array_key_exists("help", $options)) {
 	print "--file [csv file name] - Name of the CSV to be parsed (must be in same directory as script)\n";
 	print "--create_table - Build MySQL \n";
 	print "--dry_run - run all queries entered inputed but make no alterations to the database \n";
@@ -13,14 +15,28 @@ if (array_key_exists("help", getopt($shortOptions, $longOptions))) {
 	print "-h - MySQL host \n";
 	print "--help - Show this message\n";
 }
-elseif (array_key_exists("dry_run", getopt($shortOptions, $longOptions))) {
+elseif (array_key_exists("dry_run", $options)) {
 	//perform function without modifying database
 }
-elseif (array_key_exists("create_table", getopt($shortOptions, $longOptions))) {
+elseif (array_key_exists("create_table", $options)) {
 	//create table
 }
 else {
 	//run function that modifies database
+}
+
+function dbconnect($username, $password, $host, $dbname) {
+	// Create connection
+	$conn = new mysqli($host, $username, $password);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+}
+
+function dbdisconnect {
+	$conn->close();
 }
 
 ?>
